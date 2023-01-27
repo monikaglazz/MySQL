7a12d89ef11b1b3a26c0e2fe1e7e7a6bc38b0b3c
Monika Głaz <mosiex1998@gmail.com>
2023-01-27 14:32:35 +0100

    correct database_shop.sql

--- Database shop/DS - Database Shop.sql
+++ Database shop/DS - Database Shop.sql
@@ -14,11 +14,11 @@ TRUNCATE TABLE couriers;
 INSERT INTO couriers (id, courierName, city, ifAvaible, howManyParcels) 
 	VALUES ('123', 'Piter', 'Warsaw', '0', '3'),
     ('176', 'Marcin', 'Warsaw', '0', '1'),
-    ('234', 'Artur', 'Wroclaw', '1', 'NULL'),
+    ('234', 'Artur', 'Wroclaw', '1', NULL),
     ('235', 'Alicia', 'Warsaw', '0', '7'),
     ('456', 'Mario', 'Leszno', '0', '5'),
-    ('555', 'Lech', 'Cracow', '1', 'NULL'),
-    ('612', 'Piter', 'Katowice', '1', 'NULL');
+    ('555', 'Lech', 'Cracow', '1', NULL),
+    ('612', 'Piter', 'Katowice', '1', NULL);
     
 CREATE TABLE IF NOT EXISTS orders (
 	id INT NOT NULL,
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS orders (
 TRUNCATE TABLE orders;
 INSERT INTO orders (id, howManyProducts, val, ifPaid)
 	VALUES ('14', '3', '35.47', '1'),
-    ('19', '2', '89,98', '1'),
+    ('19', '2', '89.98', '1'),
     ('22', '9', '143.56', '0'),
     ('23', '12', '234.00', '1'),
     ('28', '1', '19.99', '0');
@@ -45,8 +45,8 @@ CREATE TABLE IF NOT EXISTS parcels (
     FOREIGN KEY(orderID) REFERENCES orders(id)
 );
 
-INSERT INTO parcels (id, orderID, shippmentDate, orderDate)
+INSERT INTO parcels (id, orderID, shipmentDate, orderDate)
 	VALUES ('1234', '14', '2021-03-31', '2021-04-08'),
-    ('2345', '19', '2021-04-05', 'NULL'),
+    ('2345', '19', '2021-04-05', NULL),
 	('5678', '23', '2021-04-21', '2021-04-26');
 
